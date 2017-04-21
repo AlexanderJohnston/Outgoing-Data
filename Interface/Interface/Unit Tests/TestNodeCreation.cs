58d17f0da3a5b485276b2e0fdbef45da8709acee
Alexander Johnston <alexanderj.roaming@gmail.com>
2017-04-20 23:28:20 -0400

    Added a comment regarding unit tests.

--- Interface/Interface/Unit Tests/TestNodeCreation.cs
+++ Interface/Interface/Unit Tests/TestNodeCreation.cs
@@ -9,6 +9,8 @@ namespace Interface
         [Test]
         public void House()
         {
+            // I know... I should be using NUnit and writing tests.
+            // It's the corner I regret cutting while trying to cope with a 2 person dev team.
             const string baseDir = @"\\engagests1\Elements\Prospect Jobs\Conversions\00-HOUSE_PROUSE\Completed\";
         }
     }
